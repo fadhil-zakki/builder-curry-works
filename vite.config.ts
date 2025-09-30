f056f62ef893b35757444fe3e9d3942661f9150b
Fadhil Zakki Zulva <168079274+fadhil-zakki@users.noreply.github.com>
2025-10-01 02:39:50 +0700

    Update vite.config.ts
    Signed-off-by: Fadhil Zakki Zulva <168079274+fadhil-zakki@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/builder-curry-works/",   // <--- wajib untuk GitHub Pages
   server: {
     host: "::",
     port: 8080,
